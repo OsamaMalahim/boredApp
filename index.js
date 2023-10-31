f1c4353702268367fb8269e4c1dc33d3d050de04
OsamaMalahim <engosm2009@gmail.com>
2023-10-31 22:55:17 +0300

    basic receive of api

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/", async(req,res)=>{
+    try {
+        const response = await axios.get("https://bored-api.appbrewery.com/random");
+        const result = response.data;
+        console.log(result); 
+        res.render("main.ejs",{data: result})
+    } catch (error) {
+        console.log("failed to fetch data: "+ error.message);
+        res.render("main.ejs", { error: error.message});
+    }
+});
+
+
 
 // listen
 app.listen(port,(req,res)=>{
